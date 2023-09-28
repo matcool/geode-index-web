4069e72c18e63bde3cf9ad43f680d5e8856a550f
matcool <26722564+matcool@users.noreply.github.com>
2023-09-28 17:34:34 -0300

    fetch data on client now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import ModCard from '$lib/ModCard.svelte';
-	import type { PageData } from './$types';
+	import { fetchIndex } from '$lib';
 
-	export let data: PageData;
+	const req = fetchIndex();
 </script>
 
 <main class="flex flex-col items-center">
@@ -13,9 +13,13 @@
 	<section class="my-4">
 		<input type="text" placeholder="Search for a mod" class="rounded-md px-4 py-2 shadow-md" />
 	</section>
-	<section class="flex flex-col gap-4">
-		{#each data.mods as mod (mod.id)}
-			<ModCard data={mod} />
-		{/each}
-	</section>
+	{#await req}
+		<img src="/spinner.svg" alt="spinner" />
+	{:then mods}
+		<section class="flex flex-col gap-4">
+			{#each mods as mod (mod.id)}
+				<ModCard data={mod} />
+			{/each}
+		</section>
+	{/await}
 </main>
