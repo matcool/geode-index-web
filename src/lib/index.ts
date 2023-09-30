5a4344772f2595e3f8c47fd46aa3d4d0d507e398
matcool <26722564+matcool@users.noreply.github.com>
2023-09-30 12:13:56 -0300

    implement searching

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,56 +1,62 @@
 import type { ModData } from '$lib/types';
 import { BlobReader, ZipReader, TextWriter, type Entry, Data64URIWriter } from '@zip.js/zip.js';
+import { dev } from '$app/environment';
 
 async function entryToJson(entry: Entry): Promise<any> {
-    const writer = new TextWriter();
-    return JSON.parse(await entry.getData!(writer));
+	const writer = new TextWriter();
+	return JSON.parse(await entry.getData!(writer));
 }
 
 export async function fetchIndex() {
-    const req = await fetch('https://corsproxy.io/?' + encodeURI('https://codeload.github.com/geode-sdk/mods/legacy.zip/refs/heads/main'));
-    const bytes = await req.blob();
+	const req = await fetch(
+		dev
+			? '/geode-sdk-mods.zip'
+			: 'https://corsproxy.io/?' +
+					encodeURI('https://codeload.github.com/geode-sdk/mods/legacy.zip/refs/heads/main')
+	);
+	const bytes = await req.blob();
 
-    const zipFileReader = new BlobReader(bytes);
-    const zipFile = new ZipReader(zipFileReader);
+	const zipFileReader = new BlobReader(bytes);
+	const zipFile = new ZipReader(zipFileReader);
 
-    const mods = [] as ModData[];
+	const mods = [] as ModData[];
 
-    const modEntries: { [x: string]: Entry } = {};
-    let configFile;
+	const modEntries: { [x: string]: Entry } = {};
+	let configFile;
 
-    for await (const entry of zipFile.getEntriesGenerator()) {
-        if (entry.filename.includes('/config.json')) {
-            configFile = await entryToJson(entry);
-            continue;
-        }
+	for await (const entry of zipFile.getEntriesGenerator()) {
+		if (entry.filename.includes('/config.json')) {
+			configFile = await entryToJson(entry);
+			continue;
+		}
 
-        const match = entry.filename.match(/[\w\-.]+?\/mods-v2\/([\w\-.]+?)\/(.+)/);
-        if (match == null) continue;
-        const modId = match[1];
-        const path = match[2];
-        modEntries[`${modId}/${path}`] = entry;
-    }
+		const match = entry.filename.match(/[\w\-.]+?\/mods-v2\/([\w\-.]+?)\/(.+)/);
+		if (match == null) continue;
+		const modId = match[1];
+		const path = match[2];
+		modEntries[`${modId}/${path}`] = entry;
+	}
 
-    if (configFile) {
-        for (const id of Object.keys(configFile.entries)) {
-            const latestVersion: string = configFile.entries[id].versions.at(-1);
-            const modInfoObj = await entryToJson(modEntries[`${id}/${latestVersion}/mod.json`]);
+	if (configFile) {
+		for (const id of Object.keys(configFile.entries)) {
+			const latestVersion: string = configFile.entries[id].versions.at(-1);
+			const modInfoObj = await entryToJson(modEntries[`${id}/${latestVersion}/mod.json`]);
 
-            let logoUrl;
-            if (modEntries[`${id}/logo.png`]) {
-                const writer = new Data64URIWriter();
-                logoUrl = await modEntries[`${id}/logo.png`].getData!(writer);
-            }
-            mods.push({
-                id,
-                name: modInfoObj.name,
-                logo: logoUrl ?? 'https://raw.githubusercontent.com/geode-sdk/example-mod/main/logo.png',
-                description: modInfoObj.description
-            });
-        }
-    }
+			let logoUrl;
+			if (modEntries[`${id}/logo.png`]) {
+				const writer = new Data64URIWriter();
+				logoUrl = await modEntries[`${id}/logo.png`].getData!(writer);
+			}
+			mods.push({
+				id,
+				name: modInfoObj.name,
+				logo: logoUrl ?? 'https://raw.githubusercontent.com/geode-sdk/example-mod/main/logo.png',
+				description: modInfoObj.description
+			});
+		}
+	}
 
-    await zipFile.close();
+	await zipFile.close();
 
-    return mods;
+	return mods;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,57 @@
 	import ModCard from '$lib/ModCard.svelte';
 	import { fetchIndex } from '$lib';
 	import { base } from '$app/paths';
+	import Fuse from 'fuse.js';
+	import type { ModData } from '$lib/types';
+	import { onMount } from 'svelte';
+	import debounce from 'lodash.debounce';
 
 	const req = fetchIndex();
+	let mods = [] as ModData[];
+	let filtered = [] as ModData[];
+	let fuse: Fuse<ModData> | undefined;
+	let input = '';
+	const updateSearch = debounce(() => {
+		console.log('hello!');
+		if (fuse) {
+			filtered = input ? fuse.search(input).map((x) => mods[x.refIndex]) : mods;
+		}
+	}, 100);
+
+	onMount(() => {
+		req.then((data) => {
+			filtered = mods = data;
+			fuse = new Fuse(data, {
+				keys: ['name', 'description', 'id']
+			});
+			updateSearch();
+		});
+	});
 </script>
 
-<main class="flex flex-col items-center">
+<main class="flex flex-col items-center pb-6">
 	<header class="pt-10 text-center">
 		<h1 class="text-5xl font-medium">Geode Mod Index</h1>
 		<h2>Browse through all the mods in the Geode index, on the web!</h2>
 	</header>
 	<section class="my-4">
-		<input type="text" placeholder="Search for a mod" class="rounded-md px-4 py-2 shadow-md" />
+		<input
+			type="text"
+			placeholder="Search for a mod"
+			class="rounded-md px-4 py-2 shadow-md"
+			bind:value={input}
+			on:input={updateSearch}
+		/>
 	</section>
 	{#await req}
 		<img src="{base}/spinner.svg" alt="spinner" />
-	{:then mods}
-		<section class="flex flex-col gap-4">
-			{#each mods as mod (mod.id)}
+	{:then}
+		<section class="flex flex-col gap-4 md:w-1/3">
+			{#each filtered as mod (mod.id)}
 				<ModCard data={mod} />
 			{/each}
 		</section>
+	{:catch}
+		<p class="text-lg font-medium text-red-700">Error fetching the index! Sorry</p>
 	{/await}
 </main>
